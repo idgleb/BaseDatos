9e83e499e175fd207bb471ef6256a49a97c6afc4
idgleb2 <id_gleb@mail.ru>
2024-11-04 13:23:05 -0300

    7

---
+++ BD1-TP1-GN-Ursol/consultas.sql
@@ -0,0 +1,9 @@
+select * from empleado;
+
+select * from servicio_prod order by precio;
+
+select * from caja where sucursal_id_sucursal is not null;
+
+select * from empleado where rol_id_rol=2 and telefono like "%66";
+
+
